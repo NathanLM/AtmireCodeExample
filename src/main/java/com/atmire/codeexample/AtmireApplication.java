b70be1f4224d30e2f6053e11959f076ae501e7d8
lemerrer <nlm.rennesuniv@orange.fr>
2020-02-25 11:32:58 +0100

    rename package for clarity

--- src/main/java/com/atmire/codeexample/AtmireApplication.java
+++ src/main/java/com/atmire/codeexample/AtmireApplication.java
@@ -1,4 +1,4 @@
-package com.atmire.atmire;
+package com.atmire.codeexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
