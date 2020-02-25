990538602a4cfa3a697e667b4223c5ba1b0f2c91
lemerrer <nlm.rennesuniv@orange.fr>
2020-02-25 11:01:44 +0100

    setup spring

---
+++ src/main/java/com/atmire/atmire/AtmireApplication.java
@@ -0,0 +1,13 @@
+package com.atmire.atmire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atmire/atmire/AtmireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmire.atmire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
